1a372d0609e16d678d819c46a1631a4190827e1a
fachris007 <fachrisajovi@gmail.com>
2022-06-17 06:23:47 +0700

    page info product done, sudah responsive, belum ada modal dan belum integrasi API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
+import DetailProduk from "./components/DetailProduk/DetailProduk";
 import FormLogin from "./components/FormLogin/FormLogin";
 import { FormRegistrasi } from "./components/FormRegistrasi/FormRegistrasi";
+import { InfoProduct } from "./Pages/InfoProduct/InfoProduct";
 
 function App() {
   return (
     <div className="App">
 
       {/* <FormLogin /> */}
-      <FormRegistrasi/>
+      {/* <FormRegistrasi/> */}
+      <InfoProduct/>
     </div>
   );
 }
