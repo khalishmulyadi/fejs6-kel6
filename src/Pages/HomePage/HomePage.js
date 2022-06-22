a43b421b3b2573284500159fef8d732742ac5d67
labqiwalker <labqiwalker@gmail.com>
2022-06-22 05:41:10 -0500

    -

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import CardProduct from "../../components/CardProduct/CardProduct";
+import NavbarDefault from "../../components/NavbarDefault/NavbarDefault";
+import Banner from "../../img/img banner.png";
+import slider1 from "../../img/Rectangle 129.png";
 import "./HomePage.css";
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className="container">
+      <NavbarDefault />
+      <div className="swiper"></div>
 
-export default HomePage
+      <div className="">
+        <h5>Telusuri Semua Kategori</h5>
+        <div className="kategori-search">
+          <form className="form-inline d-row">
+            <div className="group_input">
+              <div className="input-group-prepend">
+                <button
+                  className="search-btn"
+                  id="button-addon2"
+                  type="submit"
+                >
+                  <i className="bi bi-search"></i>
+                </button>
+              </div>
+              <input
+                type="text"
+                className="form-control_Custom"
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="konten row row-cols-1 row-cols-md-6">
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/routerDefault/RouterDefault.js
+++ src/routerDefault/RouterDefault.js
@@ -9,6 +9,7 @@ import { InfoProduct } from "../Pages/InfoProduct/InfoProduct";
 import { InfoProfil } from "../components/InfoProfil/InfoProfil";
 
 import Tambah from "../components/TambahProduk/Tambah";
+import HomePage from "../Pages/HomePage/HomePage";
 
 
 const RouterDefault = () => {
@@ -18,7 +19,7 @@ const RouterDefault = () => {
       <Route path="product/product-detail" element={<DetailProduk role="customer" />} />
       <Route path="product/product-preview" element={<DetailProduk role="merchant" />} />
       <Route path="product-card" element={<CardProduct />} />
-        
+      <Route path="homepage" element={<HomePage />} />
       <Route path="infoproduct" element={<InfoProduct/>} />
 
       <Route path="infoprofil" element={<InfoProfil />} />
