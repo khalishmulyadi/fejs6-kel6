68d3725ad648cd3aa54375ddeaa9a21183d2be96
Arditrn <triyanaardi3@gmail.com>
2022-07-11 19:39:35 +0700

    test tampilan

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,21 +1,16 @@
 import React from "react";
 import CardProduct from "../../components/CardProduct/CardProduct";
 import NavbarDefault from "../../components/NavbarDefault/NavbarDefault";
-import Banner from "../../img/img banner.png";
+import gitarSwiper from "../../img/gitar-swiper.png";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css/bundle";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "./HomePage.css";
 import "bootstrap/dist/css/bootstrap.css";
 const HomePage = () => {
   const slides = [];
-
-  for (let i = 0; i < 5; i += 1) {
-    slides.push(
-      <SwiperSlide key={`slide-${i}`}>
-        <img src={Banner} alt={`Slide ${i}`}></img>
-      </SwiperSlide>
-    );
-  }
 
   return (
 
@@ -23,28 +18,64 @@ const HomePage = () => {
     <div className="nav-custom">
       <NavbarDefault />
     </div>
-    <div className="container swiper-konten">
-      <React.Fragment>
-        <Swiper 
-        id="main"
-        preloadImages
-        slidesPerView={1}
-        spaceBetween
-        grabCursor
-        mousewheel
-        centeredSlides
-        >
-          {slides}
-        </Swiper>
-      </React.Fragment>
-    </div>
-
-
+    
       <div className="container section1">
         <div className="swiper-konten">
           <React.Fragment>
-            <Swiper id="main" preloadImages slidesPerView={1} spaceBetween grabCursor mousewheel centeredSlides>
-              {slides}
+          <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div className="card swiper-card">
+                  <div className="row swiper align-items-center">
+                    <div className="text-swiper-konten col-lg-6">
+                      <h3>NEW COLLECTION</h3>
+                      <h5>Gitar Listrik</h5>
+                      <p>Gitar listrik merupakan gitar yang dicolok ke listrik dan menggunakan listrik</p>
+                      {/* <button type="button" className="btn btn-secondary me-2">PESAN SEKARANG</button> */}
+                    </div>
+
+                    <div className="img-swiper-konten col-lg-6">
+                      <img src={gitarSwiper} className="card-img-side" Style="width:100%;"/>
+                    </div>
+                  </div>                  
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card swiper-card">
+                  <div className="row swiper align-items-center">
+                    <div className="text-swiper-konten col-lg-6">
+                      Slide 2
+                    </div>
+                    <div className="img-swiper-konten col-lg-6">
+                      <img src={gitarSwiper} className="card-img-side" Style="width:100%;"/>
+                    </div>
+                  </div>                  
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+              <div className="card swiper-card">
+                  <div className="row swiper align-items-center">
+                    <div className="text-swiper-konten col-lg-6 col-sm-12">
+                      Slide 3
+                    </div>
+                    <div className="img-swiper-konten col-lg-6">
+                      <img src={gitarSwiper} className="card-img-side" Style="width:100%;"/>
+                    </div>
+                  </div>                  
+                </div>
+              </SwiperSlide>
             </Swiper>
           </React.Fragment>
         </div>
@@ -65,15 +96,9 @@ const HomePage = () => {
           <button type="button" className="btn btn-secondary me-2">
             <i className="bi bi-search"></i> Aksesoris
           </button>
-          <button type="button" className="btn btn-secondary me-2">
-            <i className="bi bi-search"></i> Search
-          </button>
-          <button type="button" className="btn btn-secondary me-2">
-            <i className="bi bi-search"></i> Search
-          </button>
         </div>
 
-        <div className="konten row row-cols-2 row-cols-md-4">
+        <div className="konten">
           <CardProduct />
           <CardProduct />
           <CardProduct />
@@ -83,6 +108,12 @@ const HomePage = () => {
           <CardProduct />
           <CardProduct />
         </div>
+        
+      </div>
+      <div className="" Style="text-align: center;">
+        <button type="button" className="btn btn-secondary button-jual-homepage" Style="position: fixed; bottom:20px;">
+            <i className="bi bi-plus"></i> Jual
+        </button>
       </div>
     </div>
   );
